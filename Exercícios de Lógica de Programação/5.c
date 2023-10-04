9275fa37ffd3d516d93123b61b965026bf5ea286
Allan Bruno de Souza Bahr <madebyallanbahr@gmail.com>
2023-10-04 10:50:58 -0300

    v0.01

--- Exercícios de Lógica de Programação/5.c
+++ Exercícios de Lógica de Programação/5.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 /*
+Faça um programa em C que leia o salário fixo e o valor das vendas efetuadas pelo vendedor de uma empresa.
+Sabendo-se que o vendedor recebe uma comissão de 3% sobre o total das vendas até R$ 1.500
+mais 5% sobre o que ultrapassar este valor,
+calcular e escrever o seu salário total.
 */
+int main()
+{
+    float salario_fixo, vendas, comissao_1500, comissao_extra, salario_total;
 
-int main(){
-    
+    printf("Digite o salário fixo do vendedor\n");
+    scanf("%f", &salario_fixo);
+
+    printf("Digite o valor das vendas efetuadas pelo vendedor\n");
+    scanf("%f", &vendas);
+
+    comissao_1500 = vendas * 0.03;
+
+    if (vendas > 1500)
+    {
+        comissao_extra = (vendas - 1500) * 0.05;
+    }
+    else
+    {
+        comissao_extra = 0;
+    }
+
+    salario_total = salario_fixo + comissao_1500 + comissao_extra;
+
+    printf("O salário total do vendedor é R$ %.2f\n", salario_total);
+
+    return 0;
 }
--- Exercícios de Lógica de Programação/6.c
+++ Exercícios de Lógica de Programação/6.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
 /*
+Crie um programa em C que representa uma calculadora utilizando funções e a leitura de dados do teclado.
+Tratar denominador = 0 na operação de divisão
+Tratar valores fora do intervalo de escolha
+Adicionar o resto na operação da divisão
+Adicionar a operação de exponenciação
+
 */
 
-int main(){
-    
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num_1, num_2, operacao, resto, resultado;
+
+    printf("Digite o primeiro número\n");
+    scanf("%d", &num_1);
+
+    printf("Digite o segundo número\n");
+    scanf("%d", &num_2);
+
+    printf("Escolha a operação desejada\n1.Soma\n2.Subtração\n3.Multiplicação\n4.Divisão\n5.Exponenciação\n");
+    scanf("%d", &operacao);
+
+    switch (operacao)
+    {
+    case 1:
+        resultado = num_1 + num_2;
+        printf("1.Soma %d\n", resultado);
+        break;
+    case 2:
+        resultado = num_1 - num_2;
+        printf("2.Subtração %d\n", resultado);
+        break;
+    case 3:
+        resultado = num_1 * num_2;
+        printf("3.Multiplicação %d\n", resultado);
+    case 4:
+        if (num_2 == 0)
+        {
+            printf("Divisão não é possível");
+        }
+        else
+        {
+            resultado = num_1 / num_2;
+            resto = num_1 % num_2;
+            printf("4.Divisão %d Resto %d\n", resultado, resto);
+        }
+        break;
+    case 5:
+        resultado = pow(num_1, num_2);
+        printf("5.Exponenciação %d\n", resultado);
+        break;
+    default:
+        printf("Operação inválida\n");
+    }
+
+    return 0;
 }
